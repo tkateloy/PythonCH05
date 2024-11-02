b8b9425001db21ba2f878e84d30f27bce07fd53f
tkateloy <tkskates805@gmail.com>
2024-11-01 19:16:55 -0700

    primemodule.py

--- primemodule.py
+++ primemodule.py
@@ -1,7 +1,19 @@
 def is_prime(x):
-    for i in range(x):
+    if x <= 1:
+        return False
+    for i in range(2, x):
         if x % i == 0:
-            return False
-        else: 
-            return True
+            return False 
+    return True
     
+def print_primes(x):
+    for i in range(x):
+        if is_prime(i):
+            print(i)
+
+def get_primes(x):
+    primes = []
+    for i in range(x):
+        if is_prime(i):
+            primes.append(i)
+    return primes
